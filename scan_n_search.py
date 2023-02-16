01c212879fd7035fc68a349816de55afecfa4d33
tigercoding56 <90169211+tigercoding56@users.noreply.github.com>
2023-02-15 22:27:50 -0800

    re-add-ui

--- scan_n_search.py
+++ scan_n_search.py
@@ -104,54 +104,54 @@ def print_results(target, scan_info, port_list):
 # # welcome message
 # print("Welcome to Scan'n'Search!\nWould you like to scan a single host or multiple?")
 
-# scan_mode = input("1) Single\n2) Multiple\nq) Quit\n> ")
+ scan_mode = input("1) Single\n2) Multiple\nq) Quit\n> ")
 
-# while (scan_mode != 'q'):
-#     if scan_mode == '1':
-#         # ask for target
-#         host = input("Enter target (ex. 127.0.0.1 or scanme.nmap.org)> ")
-#         print("\nBeginning scan...\n")
+ while (scan_mode != 'q'):
+     if scan_mode == '1':
+         # ask for target
+         host = input("Enter target (ex. 127.0.0.1 or scanme.nmap.org)> ")
+         print("\nBeginning scan...\n")
 
-#         # set up the scanner
-#         nm = nmap.PortScanner()
+         # set up the scanner
+         nm = nmap.PortScanner()
 
-#         # do a lil scanning
-#         scan_info = nm.scan(host, '21-443')
+         # do a lil scanning
+         scan_info = nm.scan(host, '21-443')
 
-#         # get a list of the ports we have info for
-#         temp = list(scan_info['scan'].keys())
-#         target = temp[0]
-#         port_list = list(scan_info['scan'][target]['tcp'].keys())
+         # get a list of the ports we have info for
+         temp = list(scan_info['scan'].keys())
+         target = temp[0]
+         port_list = list(scan_info['scan'][target]['tcp'].keys())
 
-#         term_or_file = input("Where would you like your results output?\n\n1) Output to terminal\n2) Output to file\n> ")
-#         if term_or_file == '2':
-#             outfile = input("Name your output file (please include '.txt')> ")
-#             print("Saving output to file...")
-#             save_to_file(outfile, target, scan_info, port_list)
-#             print(f"Success! Output file saved to {outfile}\n")
+         term_or_file = input("Where would you like your results output?\n\n1) Output to terminal\n2) Output to file\n> ")
+         if term_or_file == '2':
+             outfile = input("Name your output file (please include '.txt')> ")
+             print("Saving output to file...")
+             save_to_file(outfile, target, scan_info, port_list)
+             print(f"Success! Output file saved to {outfile}\n")
 
-#         else:
-#             print_results(target, scan_info, port_list)
+         else:
+             print_results(target, scan_info, port_list)
 
-#     elif scan_mode == '2':
-#         print("\nComing soon!\n")
-#     else:
-#         print("\nInvalid input! Try again.\n")
-#     scan_mode = input("1) Single\n2) Multiple\nq) Quit\n> ")
+     elif scan_mode == '2':
+         print("\nComing soon!\n")
+     else:
+         print("\nInvalid input! Try again.\n")
+     scan_mode = input("1) Single\n2) Multiple\nq) Quit\n> ")
 
 # # user has pressed quit
-# print("\nThanks for using Scan'n'Search. Goodbye!")
-# input()
+print("\nThanks for using Scan'n'Search. Goodbye!")
+input()
 
-host = input("what host would you like to scan ? :")
+#host = input("what host would you like to scan ? :")
 # set up the scanner
-nm = nmap.PortScanner()
+#nm = nmap.PortScanner()
 # do a lil scanning
-scan_info = nm.scan(host, '80-443')
+#scan_info = nm.scan(host, '80-443')
 
 # get a list of the ports we have info for
-temp = list(scan_info['scan'].keys())
-target = temp[0]
+#temp = list(scan_info['scan'].keys())
+#target = temp[0]
 print(target)
 port_list = list(scan_info['scan'][target]['tcp'].keys())
 print(port_list)
